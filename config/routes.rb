b6e9ff0ed24be433088df309c230ae02e1937666
musiccity365 <m_sackstein@yahoo.com>
2021-02-18 16:41:44 -0500

    --- Auto-Git Commit ---

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'artists/resources'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs
   resources :genres
